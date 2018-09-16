93fb764fd73c989a91ef854172b4dbd4e0f75747
reddraptor <sdmuir76@gmail.com>
2018-09-16 07:31:30 -0700

    Planet trade window
    Planet trade window

---
+++ Assets/Scripts/Commodity.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "commodity", menuName = "Commodity")]
+public class Commodity : ScriptableObject {
+
+    public string label;
+    public int worth;
+}
---
+++ Assets/Scripts/CommodityStorage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "commodityStorage", menuName = "Commodity Storage")]
+public class CommodityStorage : ScriptableObject {
+
+    [System.Serializable]
+    public class CommodityStore { public Commodity commodity; public int  amount; }
+
+    public List<CommodityStore> storage;
+
+
+}
